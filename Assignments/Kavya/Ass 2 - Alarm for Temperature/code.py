1ea7c9e583324271a2af7fb4a0b4a6a4d7ea94f7
Kavya-0210 <85072435+Kavya-0210@users.noreply.github.com>
2022-09-22 19:20:21 +0530

    Update code.py

--- Assignments/Kavya/Ass 2 - Alarm for Temperature/code.py
+++ Assignments/Kavya/Ass 2 - Alarm for Temperature/code.py
@@ -5,37 +5,29 @@ Build a python code, Assume u get temperature and humidity values
 write a condition to continuously detect alarm in case of high temperature.
 '''
 
-'''
-In python, we are using random module for generating random value.  randint(start,end) func generates random vals from start to end.
-The sleep(sec) func is used to delay the output in the run time. This func is inside time module
-'''
-
 import random
 import time
 
 while(True):
 
           #temperature range in dh11 sensor is 0 to 50 Celsius
-          temp=random.randint(0,50)
+          temp=round(random.uniform(0,50),2)
           #humidity range in dh11 sensor is 20 to 90%
-          humid=random.randint(20,90)
+          humid=round(random.uniform(20,90),2)
 
           #ideal weather temperature range is 22 C to 30 C
           #ideal weather humidity range is 35% to 60%
 
           #condition for high temperature
           if(temp>30):
-                    print("High Temperature !!!")
-                    print("Temperature : %d\tHumidity : %d"%(temp,humid))
-
+                    print("High Temperature !!!\a\a\a\a\a")
+                    print("Temperature : %.2f\tHumidity : %.2f"%(temp,humid))
+                    if(humid<35 or humid>60):
+                              print("Abnormal Humidity !!!\a\a\a\a\a")
           #condition for low temperature
           if(temp<22):
-                    print("Low Temperature !!!")
-                    print("Temperature : %d\tHumidity : %d"%(temp,humid))
-
-          #condition for abnormal humidity(using logical OR operator)
-          if(humid<35 or humid>60):
-                    print("Abnormal Humidity !!!")
-                    print("Temperature : %d\tHumidity : %d"%(temp,humid))
-
+                    print("Low Temperature !!!\a\a\a\a\a")
+                    print("Temperature : %.2f\tHumidity : %.2f"%(temp,humid))
+                    if(humid<35 or humid>60):
+                              print("Abnormal Humidity !!!\a\a\a\a\a")
           time.sleep(5)
