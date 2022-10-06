9c3f97d6a190070ba9ab7822eb5128c07970c8c6
Kavya-0210 <85072435+Kavya-0210@users.noreply.github.com>
2022-10-06 20:00:37 +0530

    Update Traffic lights.py

--- Assignments/Rajeswari/Ass 3 - Python code for Raspberry pi/Traffic lights.py
+++ Assignments/Rajeswari/Ass 3 - Python code for Raspberry pi/Traffic lights.py
@@ -1,1 +1,13 @@
+from gpiozero import TrafficLights
+from time import sleep    
 
+lights = TrafficLights(25, 8, 7)    
+    
+while True:
+           light.green.on()
+           sleep(5)
+           lights.amber.on()
+           sleep(5)
+           lights.red.on()
+           sleep(5)
+           lights.off()
